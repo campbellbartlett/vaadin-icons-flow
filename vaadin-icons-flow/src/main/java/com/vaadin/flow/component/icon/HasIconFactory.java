8f2ab5c9fbfd5095f5193cab31e93f8e2ab83ff8
Campbell Bartlett <campbellb@yambay.com>
2019-02-06 21:49:13 +1100

    Fix critical sonar issue

--- vaadin-icons-flow/src/main/java/com/vaadin/flow/component/icon/HasIconFactory.java
+++ vaadin-icons-flow/src/main/java/com/vaadin/flow/component/icon/HasIconFactory.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
  * interface that allows an Icon Component to defer
  * instantiation of the Icon to it's subclass
  */
+@FunctionalInterface
 public interface HasIconFactory extends Serializable {
     Icon create();
 }
